b032be7e099e2d4126063262c2198772af0faa54
Azok42 <anton.hackner@sz-ybbs.ac.at>
2026-01-28 15:34:22 +0100

    add missing arrays and constructor

--- Publisher/Spiel.java
+++ Publisher/Spiel.java
@@ -1,14 +1,23 @@
 package Publisher;
 
+import java.util.ArrayList;
+
 public class Spiel {
     private String name;
     private double price;
     private int bewertung;
+    private ArrayList<Abo> abos;
+    private ArrayList<Bundle> bundles;
 
     public Spiel(String name, double price, int bewertung) {
         this.name = name;
         this.price = price;
         this.bewertung = bewertung;
+    }
+
+    public Spiel(String name, double price) {
+        this.name = name;
+        this.price = price;
     }
 
     public double suggestOptimalPrice() {
