920b526bfb28768908c64417d944011423f59466
Azok42 <anton.hackner@sz-ybbs.ac.at>
2026-01-28 15:26:49 +0100

    add basic structure

--- Publisher/Spiel.java
+++ Publisher/Spiel.java
@@ -1,3 +1,25 @@
 package Publisher;
 
-public class Spiel {}
+public class Spiel {
+    private String name;
+    private double price;
+    private int bewertung;
+
+    public Spiel(String name, double price, int bewertung) {
+        this.name = name;
+        this.price = price;
+        this.bewertung = bewertung;
+    }
+
+    public double suggestOptimalPrice() {
+        return 0;
+    }
+
+    public boolean checkAboOverlap(Abo newAbo) {
+        return false;
+    }
+
+    public void syncSpielPreisBundle() {
+
+    }
+}
