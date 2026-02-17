b54ddd56da19b90156610b1b89965a4707d57436
Azok42 <anton.hackner@sz-ybbs.ac.at>
2026-02-17 03:01:53 +0100

    implement recommendSpieleForKunde

--- src/Publisher/Kunde.java
+++ src/Publisher/Kunde.java
@@ -14,8 +14,68 @@ public class Kunde {
     purchases = new ArrayList<Purchase>();
   }
 
+  /**
+   * Calculates a score for all games based on owned games
+   * @return list of all games with score >= 1
+  */
   public ArrayList<Spiel> recommendSpieleForKunde() {
-    return null; //TODO: Implement recommendSpieleForKunde
+    ArrayList<String> ownedGenres = new ArrayList<>();
+    ArrayList<Publisher> ownedPublisher = new ArrayList<>();
+    double avgOwnedRating = 0;
+    int counter = 0;
+
+    for (Purchase purchase : purchases) {
+      if (purchase.getSubscription() != null)
+        continue;
+
+      if (purchase.getPurchasedGame() != null) {
+        counter++;
+        for (String genre : purchase.getPurchasedGame().getGenres()) {
+          if (!ownedGenres.contains(genre))
+            ownedGenres.add(genre);
+        }
+
+        if (!ownedPublisher.contains(purchase.getPurchasedGame().getPublisher()))
+          ownedPublisher.add(purchase.getPurchasedGame().getPublisher());
+
+        avgOwnedRating += purchase.getPurchasedGame().getBewertung();
+      }
+
+
+      if (purchase.getPurchasedBundle() != null)
+        for (Spiel game : purchase.getPurchasedBundle().getSpiele()) {
+          counter++;
+          for (String genre : game.getGenres()) {
+            if (!ownedGenres.contains(genre))
+              ownedGenres.add(genre);
+          }
+
+          if (!ownedPublisher.contains(game.getPublisher()))
+            ownedPublisher.add(game.getPublisher());
+
+          avgOwnedRating += game.getBewertung();
+        }
+    }
+    avgOwnedRating /= counter;
+
+    ArrayList<Spiel> recommendedSpiele = new ArrayList<>();
+    DataManager dataManager = DataManager.getInstance();
+
+    for (Spiel game : dataManager.getAllSpiele()) {
+      double score = 0;
+      for (String genre : game.getGenres()) {
+        if (ownedGenres.contains(genre))
+          score++;
+      }
+      score /= game.getGenres().size();
+      score += ownedPublisher.contains(game.getPublisher()) ? 1 : 0;
+      score += game.getBewertung() / 10;
+
+      if (score >= 1)
+        recommendedSpiele.add(game);
+    }
+
+    return recommendedSpiele;
   }
 
   public PublisherSale generatePersonalizedSale(Publisher publisher) {
